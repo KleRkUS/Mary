d9cdb29b8ad9b233dd26219e961c373a6839c686
KleRkUS <villevald.vladislav.sleep@gmail.com>
2021-01-10 12:16:19 +0300

    lol

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path'),
       express = require('express');
 
 const app = express();
+const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.PORT || 8080);
 
-const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
